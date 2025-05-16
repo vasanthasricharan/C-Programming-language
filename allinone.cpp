6237a66259d8ba43e31a6bc237f2f7e11a345343
Sri_Charan <154444424+vasanthasricharan@users.noreply.github.com>
2025-05-16 17:10:33 +0530

    Add files via upload

---
+++ allinone.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter any num :");
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	printf("%d\n",c);
+	c=a*b;
+	printf("%d\n",c);
+	c=a-b;
+	printf("%d\n",c);
+	return 0;
+}
